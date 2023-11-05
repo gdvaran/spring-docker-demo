df2133b1cbfbb24afd5045ff2346897af00a92a2
gdvaran <62684179+gdvaran@users.noreply.github.com>
2023-11-05 18:11:19 +0530

    add user mapping

--- src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringBootDockerApplication.java
@@ -5,13 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.*;
+import com.example.modal.User;
+import java.util.*;
+
 @RestController
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
+    List<User> users = new ArrayList<>();
+    users.add(new User(1, "tom"));
+    users.add(new User(2, "jerry"));
+
     @RequestMapping("/")
     public String home() {
         return "Hello World!";
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUser(int id) {
+        Optional<User> optional = this.users.stream().filter(user -> user.id == id).findFirst();
+        return optional.orElseThrow();
+    }
+
+    @PutMapping("/user/{id}")
+    public User updateUserName(@PathVariable int id, @RequestParam String name) {
+        Optional<User> optional = this.users.stream().filter(user -> user.id == id).findFirst();
+        optional.ifPresent(user -> {
+            user.name = name;
+        });
+        return optional.orElseThrow();
     }
 
 	public static void main(String[] args) {
